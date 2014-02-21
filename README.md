b61526a03f2e28b672150ae0bdff0edaaea598a3
Robin Grass <robin@kodlabbet.net>
2014-02-21 15:33:46 +0100

    Documentation typo.
    framweork > framework. Oops!

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Brewery
 =======
 
-[Brewery][] is an object oriented, agile and scalable [RESTful][] web applications framweork built for [PHP 5.4][] and later, which focuses on structured and enterprise-worthy application development for cloud based services and applications.
+[Brewery][] is an object oriented, agile and scalable [RESTful][] web applications framework built for [PHP 5.4][] and later, which focuses on structured and enterprise-worthy application development for cloud based services and applications.
 
 [Brewery]: http://brewphp.org/
 [PHP 5.4]: http://php.net/releases/5_4_0.php
--- libs/vendor/Brewery/Functions.php
+++ libs/vendor/Brewery/Functions.php
@@ -2,7 +2,7 @@
 /**
  *	Brewery
  *
- *	Brewery is an object oriented, agile and scalable RESTful web applications framweork built for PHP 5.4 and later, which focuses on structured and enterprise-worthy application development for cloud based services and applications.
+ *	Brewery is an object oriented, agile and scalable RESTful web applications framework built for PHP 5.4 and later, which focuses on structured and enterprise-worthy application development for cloud based services and applications.
  *
  *	@link http://brewphp.org
  *
@@ -173,7 +173,7 @@ function asset($asset) {
 
 	}
 
-	return $assetPath;
+	return "/{$assetPath}";
 
 }
 
